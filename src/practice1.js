065738fa78372588b0906aa70090e102a7fb70cd
fengpei0114 <feng_pei0114@163.com>
2020-11-10 21:46:20 +0800

    finish practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = ({data, column}) => {
+    let keys = [];
+    data.forEach(infos => {
+        let key = {};
+        column.forEach((msg, index) => {
+            key[msg.name] = infos[index];
+        })
+        keys.push(key);
+    })
+    console.log(keys);
+    return keys;
+}
+
+
 export { parseData };

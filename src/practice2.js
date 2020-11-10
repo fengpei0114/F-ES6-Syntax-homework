b91b593a2512508a926797e3b64f465ea1947c01
fengpei0114 <feng_pei0114@163.com>
2020-11-10 21:47:14 +0800

    finish practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject=(items, sections) => {
+    sections.sort((s1, s2) => s2.index - s1.index)
+        .forEach((section) => {
+            items.splice(section.index, 0, section.content);
+        });
+    return items;
+}
 export { inject };
